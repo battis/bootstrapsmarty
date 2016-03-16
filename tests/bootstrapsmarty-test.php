da9a8ba51d39d777bf431ff9fd15151cc86a171d
Seth Battis <seth@battis.net>
2016-03-16 10:26:53 -0400

    Tests

---
+++ tests/bootstrapsmarty-test.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('common.inc.php');
+
+use \Battis\BootstrapSmarty\BootstrapSmarty;
+use \Battis\BootstrapSmarty\NotificationMessage;
+
+$ui = BootstrapSmarty::getSmarty(false);
+$ui->addTemplateDir(__DIR__ .'/templates');
+
+$ui->addMessage('foo', '<a href="#">link1</a> <a class="test" href="#">link2</a> not link', NotificationMessage::ERROR);
+$ui->addMessage('foo', 'bar', NotificationMessage::GOOD);
+$ui->addMessage('<a href="#">link</a> not link', 'another message');
+
+$ui->assign('formAction', $_SERVER['PHP_SELF']);
+
+$ui->enable(BootstrapSmarty::MODULE_DATEPICKER);
+
+$ui->display("form-page.tpl");
+	
+?>
