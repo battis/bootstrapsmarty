56e99f92619ad057c9fa956df39fb22e6e825266
Seth Battis <sethbattis@stmarksschool.org>
2016-03-30 21:34:39 -0400

    Meh

--- src/BootstrapSmarty.php
+++ src/BootstrapSmarty.php
@@ -276,18 +276,7 @@ class BootstrapSmarty extends \Smarty {
 		self::testWriteableDirectory($this->getCacheDir());
 		
 		/* set some reasonable defaults */
-		$this->url =
-			(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on' ?
-	                'http://' :
-	                'https://'
-	        ) .
-	        $_SERVER['SERVER_NAME'] .
-	        $_SERVER['CONTEXT_PREFIX'] .
-	        str_replace(
-		        $_SERVER['CONTEXT_DOCUMENT_ROOT'],
-		        '',
-		        dirname(__DIR__)
-	        );
+		$this->url = DataUtilities::URLfromPath(dirname(__DIR__));
 		$this->assign('BOOTSTRAPSMARTY_URL', $this->url);
 		$this->addStylesheet("{$this->url}/css/BootstrapSmarty.css", self::UI_KEY);
 		$this->assign('name', DataUtilities::titleCase(preg_replace('/[\-_]+/', ' ', urldecode(basename($_SERVER['REQUEST_URI'], '.php')))));
